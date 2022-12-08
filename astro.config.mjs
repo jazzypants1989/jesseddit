ac02919220340e35f15c6a745c9dcef49fab0a8d
Jesse Pence <jessepence@gmail.com>
2022-12-08 14:28:11 -0800

    i am sad

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 import mdx from "@astrojs/mdx"
-import vercel from "@astrojs/vercel/serverless"
+import netlify from "@astrojs/netlify/functions"
 
+// https://astro.build/config
+import node from "@astrojs/node"
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs(), mdx()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
 })
