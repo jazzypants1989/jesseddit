01afb69b1981e2acd9d874fd46a3d4f59a3a0b53
Jesse Pence <jessepence@gmail.com>
2022-12-08 01:07:06 -0800

    here goes everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
-import compress from "astro-compress"
 import solidJs from "@astrojs/solid-js"
 import mdx from "@astrojs/mdx"
 import vercel from "@astrojs/vercel/serverless"
 
 export default defineConfig({
-  integrations: [tailwind(), compress(), solidJs(), mdx()],
+  integrations: [tailwind(), solidJs(), mdx()],
   output: "server",
   adapter: vercel(),
 })
